0ededd165ceff159e1595e1a9e54f2c8b817ca02
saviinda <savi077nda@gmail.com>
2026-01-27 15:51:07 +0530

    DashboardStatusResponse renamed

--- backend/src/main/java/com/hospital/queue/dto/DashboardStatusResponse.java
+++ backend/src/main/java/com/hospital/queue/dto/DashboardStatusResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class DashboardStatsResponse {
+public class DashboardStatusResponse {
     private Long totalTokensToday;
     private Long completedTokens;
     private Long waitingTokens;
