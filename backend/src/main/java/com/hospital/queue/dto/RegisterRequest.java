8ca4b0a656fc513ca30f1630ea4d31f9e8e38482
saviinda <savi077nda@gmail.com>
2026-01-27 15:51:57 +0530

    RegisterRequest dto coded

---
+++ backend/src/main/java/com/hospital/queue/dto/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.hospital.queue.dto;
+
+import com.hospital.queue.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    private String phoneNumber;
+
+    @NotNull(message = "Role is required")
+    private User.Role role;
+
+    private Long hospitalId;
+}
