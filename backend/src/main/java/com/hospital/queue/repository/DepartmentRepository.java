73deb67e34efea05fc3752f8eeb71cd3018a3490
saviinda <savi077nda@gmail.com>
2026-01-27 15:28:14 +0530

    DepartmentRepo coded

--- backend/src/main/java/com/hospital/queue/repository/DepartmentRepository.java
+++ backend/src/main/java/com/hospital/queue/repository/DepartmentRepository.java
@@ -1,4 +1,11 @@
 package com.hospital.queue.repository;
 
-public interface DepartmentRepository {
+import com.hospital.queue.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    List<Department> findByHospitalId(Long hospitalId);
 }
